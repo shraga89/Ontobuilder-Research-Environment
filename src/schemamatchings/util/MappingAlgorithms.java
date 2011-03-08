c3c7fbf0a0a15beca8dcd689d350b7244d7f23cf
tomers77 <tomers77@localhost>
2011-03-08 09:40:19 +0200

    cleanup

--- src/schemamatchings/util/MappingAlgorithms.java
+++ src/schemamatchings/util/MappingAlgorithms.java
@@ -19,14 +19,14 @@ public interface MappingAlgorithms {
   public static  String MAX_WEIGHT_BIPARTITE_GRAPH_STABLE_MARRIAGE_INTERSECTION = "Intersection";
   public static  String MAX_WEIGHT_BIPARTITE_GRAPH_STABLE_MARRIAGE_UINION = "Union";
   /*public static  String BETA_DIST_MODEL_MAPPING = "Beta Distribution Model";
-  public static  String FILTERED_MAX_WEIGHT_MAPPING = "Filtered Max Weighted Bipartite Graph";*/
-  public static  String NAIVE_BAYES = "Naive Bayes";
+  public static  String FILTERED_MAX_WEIGHT_MAPPING = "Filtered Max Weighted Bipartite Graph";
+  public static  String NAIVE_BAYES = "Naive Bayes"; */
   public static  String[] ALL_ALGORITHM_NAMES = {MAX_WEIGHT_BIPARTITE_GRAPH,
                                                  STABLE_MARRIAGE,
                                                  DOMINANTS,
                                                  MAX_WEIGHT_BIPARTITE_GRAPH_STABLE_MARRIAGE_INTERSECTION,
-                                                 MAX_WEIGHT_BIPARTITE_GRAPH_STABLE_MARRIAGE_UINION ,
-                                                 NAIVE_BAYES
-                                                 /*BETA_DIST_MODEL_MAPPING,FILTERED_MAX_WEIGHT_MAPPING,NAIVE_BAYES*/};
+                                                 MAX_WEIGHT_BIPARTITE_GRAPH_STABLE_MARRIAGE_UINION /*,
+                                                 NAIVE_BAYES,
+                                                 BETA_DIST_MODEL_MAPPING,FILTERED_MAX_WEIGHT_MAPPING,NAIVE_BAYES*/};
 
 }
