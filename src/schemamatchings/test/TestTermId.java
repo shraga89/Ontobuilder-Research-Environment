f4981557c4bd12465b6e12ae1e4f4f6e7763165a
tomers77 <tomers77@localhost>
2011-06-12 10:46:10 +0300

    cleanup

--- src/schemamatchings/test/TestTermId.java
+++ src/schemamatchings/test/TestTermId.java
@@ -15,21 +15,18 @@ import com.modica.ontology.match.MatchInformation;
 public class TestTermId {
 	public static void main(String[] args) throws Exception{
 		OntoBuilderWrapper obw = new OntoBuilderWrapper();
-		String folder = "C:\\AviTest";
-		Ontology o1 = obw.readOntologyXMLFile(folder+"\\"+"dbr.xml",true);
-		o1.getModel().setLight(true);
-		o1.save(new File(folder+"\\"+"dbr.xml"));
-		Ontology o2 = obw.readOntologyXMLFile(folder+"\\"+"dbs.xml",true);
-		o2.getModel().setLight(true);
-		o2.save(new File(folder+"\\"+"dbs.xml"));
+		String folder = "D:\\Ontology_Pairs_and_Exact_Mappings\\Ontology Pairs and Exact Mappings\\3-national_geographic.xml_6-people.xml_EXACT";
+		Ontology o1 = obw.readOntologyXMLFile(folder+"\\"+"3-national_geographic.xml",true);
+		o1.save(new File(folder+"\\"+"3-national_geographic.xml"));
+		Ontology o2 = obw.readOntologyXMLFile(folder+"\\"+"6-people.xml",true);
+		o2.save(new File(folder+"\\"+"6-people.xml"));
 		MatchInformation mi = obw.matchOntologies(o1,o2,MatchingAlgorithms.TERM);
 		SchemaTranslator st = new SchemaTranslator();
 		st.importIdsFromMatchInfo(mi,true);
 		MatchMatrix mm = mi.getMatrix();
-		st.saveMatchToXML(0, "o1", "o2", "C:\\AviTest\\Match.xml");
-		SchemaTranslator exact = SchemaMatchingsUtilities.readXMLBestMatchingFile(folder+"\\"+"dbr_EXACT_dbs.xml",mm);
-		exact.saveMatchToXML(0, "o1", "o2", "C:\\dbr_dbs_EXACT.xml");
-	
+		st.saveMatchToXML(0, "o1", "o2", "d:\\match.xml");
+		SchemaTranslator exact = SchemaMatchingsUtilities.readXMLBestMatchingFile(folder+"\\"+"3-national_geographic.xml_6-people.xml_EXACT.xml",mm);
+		exact.saveMatchToXML(0, "o1", "o2", "d:\\match3.xml");
     	System.out.println("P: "+SchemaMatchingsUtilities.calculatePrecision(st, exact)+", R: "+SchemaMatchingsUtilities.calculateRecall(st, exact));
 	}
 }
