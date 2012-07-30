8c22f6b861b51f71341117867e265687e949c4f4
tomers77 <tomers77@localhost>
2012-07-30 13:34:59 +0300

    cleanup

--- src/ac/technion/schemamatching/matchers/secondline/SLMList.java
+++ src/ac/technion/schemamatching/matchers/secondline/SLMList.java
@@ -12,8 +12,11 @@ import java.util.HashMap;
  */
 public enum SLMList {OBMWBG(new OBmwbg()),OBSM(new OBStableMarriage()),
 	OBDom(new OBDominants()),OBIntersection(new OBIntersection()),
-	OBUnion(new OBUnion()),OBThreshold015(new OBThreshold(0.15)),OBThreshold025(new OBThreshold(0.25)),OBThreshold050(new OBThreshold(0.50)),OBThreshold075(new OBThreshold(0.75)),
-	OBTopK(new OBTopK()), OBMax(new OBMaxDelta(0.0)),OBMaxDelta005(new OBMaxDelta(0.05)), OBMaxDelta01(new OBMaxDelta(0.1)), OBMaxDelta02(new OBMaxDelta(0.2));
+	OBUnion(new OBUnion()),OBThreshold015(new OBThreshold(0.15)),
+	OBThreshold025(new OBThreshold(0.25)),
+	OBThreshold050(new OBThreshold(0.50)),OBThreshold075(new OBThreshold(0.75)),
+	OBTopK(new OBTopK()), OBMax(new OBMaxDelta(0.0)),OBMaxDelta005(new OBMaxDelta(0.05)), 
+	OBMaxDelta01(new OBMaxDelta(0.1)), OBMaxDelta02(new OBMaxDelta(0.2));
 
 private SLMList(SecondLineMatcher slm)
 {
