35c0b305d8f79144dbb5f25ca62e109988225da6
matthiasweidlich <weidlich@tx.technion.ac.il>
2013-03-12 16:36:04 +0200

    process model matchers

---
+++ src/ac/technion/schemamatching/experiments/pairwise/ProcessModelPrediction.java
@@ -0,0 +1,54 @@
+package ac.technion.schemamatching.experiments.pairwise;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+import ac.technion.iem.ontobuilder.matching.match.MatchInformation;
+import ac.technion.schemamatching.experiments.OBExperimentRunner;
+import ac.technion.schemamatching.matchers.firstline.FLMList;
+import ac.technion.schemamatching.matchers.firstline.FirstLineMatcher;
+import ac.technion.schemamatching.matchers.firstline.ProcessModelMatcher;
+import ac.technion.schemamatching.matchers.firstline.ProcessModelMatcher.ProcessModelMatchStrategy;
+import ac.technion.schemamatching.matchers.secondline.SecondLineMatcher;
+import ac.technion.schemamatching.statistics.Statistic;
+import ac.technion.schemamatching.testbed.ExperimentSchemaPair;
+
+public class ProcessModelPrediction implements PairWiseExperiment {
+
+	@Override
+	public List<Statistic> runExperiment(ExperimentSchemaPair esp) {
+		
+		HashMap<String,MatchInformation> flMatches = new HashMap<String,MatchInformation>(); 
+		ProcessModelMatcher flm = new ProcessModelMatcher();
+		
+		for (ProcessModelMatchStrategy strategy : ProcessModelMatchStrategy.values()) {
+			System.out.println("Do: " + strategy);
+			flm.setMatchingStrategy(strategy);
+			flMatches.put(strategy.toString(),flm.match(esp.getCandidateOntology(), esp.getTargetOntology(), false));
+			System.out.println("Done.");
+		}
+		
+		return null;
+	}
+
+	@Override
+	public boolean init(OBExperimentRunner oer, Properties properties,
+			ArrayList<FirstLineMatcher> flM, ArrayList<SecondLineMatcher> slM) {
+		
+		return false;
+	}
+
+	@Override
+	public String getDescription() {
+		return "Experiment on predicting the performance of various process model matchers";
+	}
+
+	@Override
+	public List<Statistic> summaryStatistics() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/ac/technion/schemamatching/matchers/firstline/ProcessModelMatcher.java
@@ -0,0 +1,163 @@
+package ac.technion.schemamatching.matchers.firstline;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import matching.searchers.predict.OptSEDOverAttributeLabels;
+import matching.searchers.predict.PredictionSearcher;
+import matching.searchers.predict.StandardVirtualDocForComponentPredictionSearcher;
+import matching.searchers.predict.StandardVirtualDocForDistanceSetPredictionSearcher;
+import nl.tue.tm.is.graph.Graph;
+import nl.tue.tm.is.graph.TwoValuedVertices;
+import nl.tue.tm.is.labelAnalyzer.interfaces.SemanticLanguage;
+import nl.tue.tm.is.labelAnalyzer.interfaces.SemanticLanguage.LanguageCode;
+import nl.tue.tm.is.ptnet.PTNet;
+import ac.technion.iem.ontobuilder.core.ontology.Ontology;
+import ac.technion.iem.ontobuilder.core.ontology.Term;
+import ac.technion.iem.ontobuilder.matching.match.MatchInformation;
+import ac.technion.schemamatching.matchers.MatcherType;
+
+public class ProcessModelMatcher implements FirstLineMatcher {
+
+	public enum ProcessModelMatchStrategy {
+		// textual similarities
+		OptSEDOverActivityLabels,
+		OptSEDOverRoles,
+		OptSEDOverData,
+//		LinOverActivities,
+//		LinOverActivitiesWithObjects,
+//		LinOverActionsOfActivities,
+//		LinOverObjectsOfActivities,
+		VirtualDocs,
+		VirtualDocsDistanceSet,
+		VirtualDocsTreeSet,
+		
+		// textual syntax/organisational/entity-based (SED over roles)
+		// textual semantic/functional/partial model (Lin or similar over activities)
+		// structural/control flow/full model (GED over graphs)
+		// behavioural/control flow/partial model (BP over sub-graph)
+		// textual semantic/data/entity-based (some wordnet relation for object names)
+	}
+	
+	private ProcessModelMatchStrategy currentStrategy = ProcessModelMatchStrategy.OptSEDOverActivityLabels;
+	
+	private LanguageCode languageCode = LanguageCode.EN;
+	
+	private String languageString = "english";
+	
+	
+	@Override
+	public String getName() {
+		return "Process Model Matcher";
+	}
+
+	@Override
+	public boolean hasBinary() {
+		return false;
+	}
+	
+	public void setMatchingStrategy(ProcessModelMatchStrategy strategy) {
+		this.currentStrategy = strategy;
+	}
+	
+	public void setMatchingStrategy(ProcessModelMatchStrategy strategy, LanguageCode languageCode) {
+		this.currentStrategy = strategy;
+		this.languageCode = languageCode;
+		
+		switch (this.languageCode){
+		case NL:
+			this.languageString = "dutch";
+			break;
+		default:
+			this.languageString = "english";
+		}
+	}
+
+	@Override
+	public MatchInformation match(Ontology candidate, Ontology target,
+			boolean binary) {
+		
+		PredictionSearcher searcher = null;
+		Set<String> attributes = new HashSet<>();
+		SemanticLanguage.setLanguage(this.languageCode);
+		
+		switch (this.currentStrategy) {
+		case OptSEDOverActivityLabels:
+			searcher = new OptSEDOverAttributeLabels(Graph.ATTRIBUTE_LABEL, this.languageString);
+			break;
+		case OptSEDOverRoles:
+			attributes.add(Graph.ATTRIBUTE_UITVOERENDE);
+			attributes.add(Graph.ATTRIBUTE_RESPONSIBLE);
+			searcher = new OptSEDOverAttributeLabels(attributes, this.languageString);
+			break;
+
+		case OptSEDOverData:
+			attributes.add(Graph.ATTRIBUTE_DATA);
+			attributes.add(Graph.ATTRIBUTE_DATA_OBJECTEN);
+			attributes.add(Graph.ATTRIBUTE_INPUTS);
+			attributes.add(Graph.ATTRIBUTE_OUTPUTS);
+			searcher = new OptSEDOverAttributeLabels(attributes, this.languageString);
+			break;
+			
+		case VirtualDocs:
+			searcher = new StandardVirtualDocForDistanceSetPredictionSearcher(0);
+			break;
+		case VirtualDocsDistanceSet:
+			searcher = new StandardVirtualDocForDistanceSetPredictionSearcher(1);
+			break;
+		case VirtualDocsTreeSet:
+			searcher = new StandardVirtualDocForComponentPredictionSearcher(1);
+			break;
+			
+		default:
+			System.err.println("No searcher selected");
+			break;
+		}
+		
+		Graph sg1 = loadGraphFromPNML(candidate.getFile().getPath());
+		Graph sg2 = loadGraphFromPNML(target.getFile().getPath());
+
+		MatchInformation result = new MatchInformation(candidate, target);
+		
+		
+		
+		//Initialize solution mappings
+		Set<TwoValuedVertices> solutions = searcher.search(sg1, sg2); 
+		for (TwoValuedVertices tvs: solutions){
+			Term candidateTerm = candidate.getTermByProvenance(sg1.getLabel(tvs.v1));
+			Term targetTerm = target.getTermByProvenance(sg2.getLabel(tvs.v2));
+			if (candidateTerm == null || targetTerm == null) {
+				System.err.println("Term not found...");
+				candidateTerm = candidate.getTermByProvenance(sg1.getLabel(tvs.v1));
+				targetTerm = target.getTermByProvenance(sg2.getLabel(tvs.v2));
+				
+			}
+			result.updateMatch(targetTerm, candidateTerm, tvs.v);
+		}
+
+		return result;
+	}
+
+	@Override
+	public String getConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MatcherType getType() {
+		return null;
+	}
+
+	@Override
+	public int getDBid() {
+		return 17;
+	}
+	
+	private Graph loadGraphFromPNML(String filename){
+		PTNet ptnet = PTNet.loadPNML(filename);
+		Graph sg = new Graph(ptnet);
+		return sg;
+	}
+
+}
