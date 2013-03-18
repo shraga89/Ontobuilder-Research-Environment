6edfe9c8f94b2c97323db34aba96812a37888925
matthiasweidlich <weidlich@tx.technion.ac.il>
2013-03-18 18:49:55 +0200

    minor update ppm exp

--- src/ac/technion/schemamatching/experiments/pairwise/ProcessModelPrediction.java
+++ src/ac/technion/schemamatching/experiments/pairwise/ProcessModelPrediction.java
@@ -4,11 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import org.jbpt.bp.BehaviouralProfile;
-import org.jbpt.bp.construct.BPCreatorUnfolding;
-import org.jbpt.petri.NetSystem;
-import org.jbpt.petri.Node;
-
 import matching.virtualdoc.VirtualDocParams;
 import nl.tue.tm.is.labelAnalyzer.interfaces.SemanticLanguage;
 import nl.tue.tm.is.labelAnalyzer.interfaces.SemanticLanguage.LanguageCode;
@@ -20,6 +15,7 @@ import ac.technion.schemamatching.matchers.firstline.ProcessModelCompleteMatcher
 import ac.technion.schemamatching.matchers.firstline.ProcessModelFLM;
 import ac.technion.schemamatching.matchers.firstline.ProcessModelFLM.ProcessModelMatchStrategy;
 import ac.technion.schemamatching.matchers.secondline.SecondLineMatcher;
+import ac.technion.schemamatching.statistics.AttributeNBGolden;
 import ac.technion.schemamatching.statistics.BinaryGolden;
 import ac.technion.schemamatching.statistics.DummyStatistic;
 import ac.technion.schemamatching.statistics.K2Statistic;
@@ -64,69 +60,74 @@ public class ProcessModelPrediction implements PairWiseExperiment {
 		VirtualDocParams.useStemming = true;
 		
 		
-//		/*
-//		 * 1. Check the actual results obtained with the complete matchers
-//		 */
-//		ProcessModelCompleteMatchers completeMatchers = new ProcessModelCompleteMatchers();
-//		for(ProcessModelMatcher matcher : ProcessModelMatcher.values()) {
-//			System.out.println("Complete Matcher: " + matcher.toString());
-//			
-//			completeMatchers.setMatchingStrategy(matcher);
-//			
-//			MatchInformation mi = completeMatchers.match(esp.getCandidateOntology(), esp.getTargetOntology(), true);
-//			
-//			K2Statistic binary = new BinaryGolden();
-//			String instanceDesc = esp.getID()+","+completeMatchers.getName();
-//			binary.init(instanceDesc, mi, esp.getExact());
-//			results.add(binary);
-//		}
-//		
-//		/*
-//		 * 2. Consider all process model sim measures
-//		 */
-//		ProcessModelFLM flm = new ProcessModelFLM();
-//		for (ProcessModelMatchStrategy strategy : ProcessModelMatchStrategy.values()) {
-//			System.out.println("FLM: " + strategy.toString());
-//			flm.setMatchingStrategy(strategy, languageCode);
-//			MatchInformation miFirst = flm.match(esp.getCandidateOntology(), esp.getTargetOntology(), false);
-//			
-//			/*
-//			 * 2.1 Non-Binary results for the sim measure
-//			 */
-//			K2Statistic nonBinary = new NBGolden();
-//			String instanceDesc = esp.getID()+","+flm.getName();
-//			nonBinary.init(instanceDesc, miFirst, esp.getExact());
-//			results.add(nonBinary);
-//			
-//			/*
-//			 * 2.2 All matrix and attribute predictors for the sim measure
-//			 */
-//			System.out.println("FLM Matrix");
-//			Statistic  mPred = new MatrixPredictors();
-//			instanceDesc = esp.getID()+","+flm.getName();
-//			mPred.init(instanceDesc, miFirst);
-//			results.add(mPred);
-//			
-//			Statistic  aPred = new AttributePredictors();
-//			instanceDesc = esp.getID()+","+flm.getName();
-//			aPred.init(instanceDesc, miFirst);
-//			results.add(aPred);
-//
-//			/*
-//			 * 2.3 Binary result for a selection of SLMs
-//			 */
-//			System.out.print("FLM with SLM ");
-//			for (SecondLineMatcher sMatcher : this.slM) {
-//				System.out.print(sMatcher.getName() + " ");
-//				MatchInformation miSecond = sMatcher.match(miFirst);
-//
-//				K2Statistic binary = new BinaryGolden();
-//				instanceDesc = esp.getID()+","+ flm.getName()+"_"+sMatcher.getName();
-//				binary.init(instanceDesc, miSecond, esp.getExact());
-//				results.add(binary);
-//			}
-//			System.out.println("");
-//		}
+		/*
+		 * 1. Check the actual results obtained with the complete matchers
+		 */
+		ProcessModelCompleteMatchers completeMatchers = new ProcessModelCompleteMatchers();
+		for(ProcessModelMatcher matcher : ProcessModelMatcher.values()) {
+			System.out.println("Complete Matcher: " + matcher.toString());
+			
+			completeMatchers.setMatchingStrategy(matcher);
+			
+			MatchInformation mi = completeMatchers.match(esp.getCandidateOntology(), esp.getTargetOntology(), true);
+			
+			K2Statistic binary = new BinaryGolden();
+			String instanceDesc = esp.getID()+","+completeMatchers.getName();
+			binary.init(instanceDesc, mi, esp.getExact());
+			results.add(binary);
+		}
+		
+		/*
+		 * 2. Consider all process model sim measures
+		 */
+		ProcessModelFLM flm = new ProcessModelFLM();
+		for (ProcessModelMatchStrategy strategy : ProcessModelMatchStrategy.values()) {
+			System.out.println("FLM: " + strategy.toString());
+			flm.setMatchingStrategy(strategy, languageCode);
+			MatchInformation miFirst = flm.match(esp.getCandidateOntology(), esp.getTargetOntology(), false);
+			
+			/*
+			 * 2.1 Non-Binary results for the sim measure, also on attribute level
+			 */
+			K2Statistic nonBinary = new NBGolden();
+			String instanceDesc = esp.getID()+","+flm.getName();
+			nonBinary.init(instanceDesc, miFirst, esp.getExact());
+			results.add(nonBinary);
+			
+			K2Statistic  nonBinaryAtt = new AttributeNBGolden();
+			instanceDesc = esp.getID()+","+flm.getName();
+			nonBinaryAtt.init(instanceDesc, miFirst, esp.getExact());
+			results.add(nonBinaryAtt);
+			
+			/*
+			 * 2.2 All matrix and attribute predictors for the sim measure
+			 */
+			System.out.println("FLM Matrix");
+			Statistic  mPred = new MatrixPredictors();
+			instanceDesc = esp.getID()+","+flm.getName();
+			mPred.init(instanceDesc, miFirst);
+			results.add(mPred);
+			
+			Statistic  aPred = new AttributePredictors();
+			instanceDesc = esp.getID()+","+flm.getName();
+			aPred.init(instanceDesc, miFirst);
+			results.add(aPred);
+
+			/*
+			 * 2.3 Binary result for a selection of SLMs
+			 */
+			System.out.print("FLM with SLM ");
+			for (SecondLineMatcher sMatcher : this.slM) {
+				System.out.print(sMatcher.getName() + " ");
+				MatchInformation miSecond = sMatcher.match(miFirst);
+
+				K2Statistic binary = new BinaryGolden();
+				instanceDesc = esp.getID()+","+ flm.getName()+"_"+sMatcher.getName();
+				binary.init(instanceDesc, miSecond, esp.getExact());
+				results.add(binary);
+			}
+			System.out.println("");
+		}
 		
 		/*
 		 * 3. Consider all process model property predictors
