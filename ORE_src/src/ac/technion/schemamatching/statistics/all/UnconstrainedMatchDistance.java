e9e00778f86f9005ddeba2cc92dc2498e4207f65
tomers77 <tomers77@localhost>
2014-02-17 16:17:27 +0200

    new NBNB experiment

--- ORE_src/src/ac/technion/schemamatching/statistics/all/UnconstrainedMatchDistance.java
+++ ORE_src/src/ac/technion/schemamatching/statistics/all/UnconstrainedMatchDistance.java
@@ -23,16 +23,17 @@ import ac.technion.iem.ontobuilder.matching.match.MatchInformation;
  * Both functions are reversed to retain the intuitive 
  * interpretation where a higher result is preferred. 
  * Since normalization expressions are based upon the task's 
- * exact match vectors, they remain constant between 
+ * reference vector, they remain constant between 
  * different match results and thus the functions retain 
- * the sub-additivity property of distance measures.
- * Assumes exact-match vectors are binary. 
- *
+ * the sub-additivity property of distance measures. 
+ * Unconstrained version of @link{MatchDistance} allowing 
+ * non-binary reference vectors. 
  */
-public class MatchDistance implements K2Statistic {
+public class UnconstrainedMatchDistance implements K2Statistic {
 
-	String[] header = new String[]{"instance","MD","NMD"};
+	String[] header = new String[]{"instance","NED","UMD","UNMD"};
 	private List<String[]> data = new ArrayList<String[]>();
+	private double threshold = 0.5; //Threshold for consideration in "Match" dimensions.  
 	/* (non-Javadoc)
 	 * @see ac.technion.schemamatching.statistics.Statistic#getHeader()
 	 */
@@ -46,7 +47,7 @@ public class MatchDistance implements K2Statistic {
 	 */
 	@Override
 	public String getName() {
-		return "Match Distance";
+		return "Unconstrained Match Distance";
 	}
 
 	/* (non-Javadoc)
@@ -71,30 +72,63 @@ public class MatchDistance implements K2Statistic {
 	@Override
 	public boolean init(String instanceDescription, MatchInformation mi,
 			MatchInformation exactMatch) {
-		String[] res = new String[3];
+		String[] res = new String[4];
 		res[0] = instanceDescription;
 		Set<Match> exact = new HashSet<Match>(exactMatch.getCopyOfMatches());
-		double mdDenom = Math.sqrt((double)exact.size());
-		double nmdDenom = Math.sqrt((double)((long)exactMatch.getCandidateOntologyTermsTotal()*(long)exactMatch.getTargetOntologyTermsTotal()-exact.size()));
+		double mdDenom = 0.0;
+		double nmdDenom = (double)((long)exactMatch.getCandidateOntologyTermsTotal() //TODO: fix for partial vectors MD
+				*(long)exactMatch.getTargetOntologyTermsTotal()-mi.getNumMatches());
 		double mdDist = 0.0;
 		double nmdDist = 0.0;
+		double dist = 0.0;
 		for (Match m : mi.getCopyOfMatches())
 		{
 			double eVal = exactMatch.getMatchConfidence(m.getCandidateTerm(), m.getTargetTerm());
 			if (eVal>0.0) 
 				exact.remove(m);
 			double val = m.getEffectiveness(); 
-			mdDist+=Math.pow(eVal-eVal*val,2.0);
-			nmdDist+=Math.pow((1-eVal)*eVal-(1-eVal)*val,2.0);
+			if (eVal>=threshold)
+			{
+				mdDist+=Math.pow(val-eVal,2.0) ;
+				mdDenom+=Math.pow(eVal,2.0);
+				nmdDenom-=1.0;
+			}
+			else
+			{
+				nmdDist+=Math.pow(val-eVal,2.0) ;
+				nmdDenom+=Math.pow(1.0-eVal,2.0);
+			}
+			dist +=Math.pow(val-eVal,2.0) ;
 		}
-		//Add 1 distances for unmatched exact matches
-		mdDist+=(double)exact.size();
+		//Add distances for unmatched exact matches
+		for (Match e : exact)
+		{
+			double eVal = e.getEffectiveness(); 
+			if (eVal>=threshold)
+			{
+				mdDist+=Math.pow(eVal,2.0) ;
+				mdDenom+=Math.pow(eVal,2.0);
+				nmdDenom-=1.0;
+			}
+			else
+			{
+				nmdDist+=Math.pow(eVal,2.0) ;
+				nmdDenom+=Math.pow(1-eVal,2.0);
+			}
+			dist +=Math.pow(eVal,2.0) ;
+		}
+		
 		
 		//finish
-		double md = 1.0-(mdDenom ==0 ? 1.0 : Math.sqrt(mdDist)/mdDenom); 
-		double nmd = 1.0-(nmdDenom ==0 ? 1.0 : Math.sqrt(nmdDist)/nmdDenom);
-		res[1] = Double.toString(md);
-		res[2] = Double.toString(nmd);
+		double md = 1.0-(mdDenom ==0 ? 1.0 : Math.sqrt(mdDist)/Math.sqrt(mdDenom)); 
+		double nmd = 1.0-(nmdDenom ==0 ? 1.0 : Math.sqrt(nmdDist)/Math.sqrt(nmdDenom));
+		double d = 1.0 - (mdDenom + nmdDenom==0 ? 1.0 : 
+			Math.sqrt(dist)/ 
+			Math.sqrt(mdDenom+nmdDenom));
+		res[1] = Double.toString(d);
+		res[2] = Double.toString(md);
+		res[3] = Double.toString(nmd);
+		
 		data.add(res);
 		return true;
 	}
