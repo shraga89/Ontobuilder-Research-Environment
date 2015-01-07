dee27f6f2dab544a8678670066bd46988ae1c3bc
tomers77 <tomers77@localhost>
2015-01-07 09:41:31 +0200

    New Max Delta

--- src/ac/technion/schemamatching/statistics/predictors/STDEVPredictor.java
+++ src/ac/technion/schemamatching/statistics/predictors/STDEVPredictor.java
@@ -5,16 +5,20 @@ import java.util.ArrayList;
 
 
 /**
- * Calculates the population standard deviation over the vector
+ * Calculates the population standard deviation over the vector 
+ * each row at a time and returns the average
  * @author Tomer Sagi
  *
  */
 public class STDEVPredictor implements Predictor{
 
 	private double sum;
+	private long len;
+	private double rowSum = 0.0;
+	private double rowLen = 0.0;
 	private double zeros;
 	private ArrayList<Double> nonZeros;
-	private long len;
+	
 	
 	public String getName() 
 	{
@@ -23,12 +27,28 @@ public class STDEVPredictor implements Predictor{
 
 	public void newRow() 
 	{
+		if (rowLen>0)
+		{
+			double res = 0.0;
+			double avg = rowSum/rowLen;
+			for (Double nonZ : nonZeros)
+			{
+				res+=Math.pow(nonZ-avg, 2);
+			}
+			res+=zeros*Math.pow(avg, 2);
+			sum+=Math.sqrt(res/rowLen);
+			len++;
+		}
+		rowSum=0.0;
+		rowLen=0.0;
+		nonZeros = new ArrayList<Double>();
+		zeros= 0;
 		
 	}
 
 	public void visitColumn(double val) {
-		sum+=val;
-		len++;
+		rowSum+=val;
+		rowLen++;
 		if (val>0)
 			nonZeros.add(new Double(val));
 		else
@@ -45,14 +65,18 @@ public class STDEVPredictor implements Predictor{
 
 	public double getRes() 
 	{
-		double res = 0.0;
-		double avg = sum/len;
-		for (Double nonZ : nonZeros)
-		{
-			res+=Math.pow(nonZ-avg, 2);
-		}
-		res+=zeros*Math.pow(avg, 2);
-		return Math.sqrt(res/len);
+		newRow();
+//		double res = 0.0;
+//		double avg = sum/len;
+//		for (Double nonZ : nonZeros)
+//		{
+//			res+=Math.pow(nonZ-avg, 2);
+//		}
+//		res+=zeros*Math.pow(avg, 2);
+//		return Math.sqrt(res/len);
+		if (len==0)
+			return 0;
+		return sum/len;
 	}
 
 }
